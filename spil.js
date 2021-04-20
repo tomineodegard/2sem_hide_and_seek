60aafe37dff67aea270eaa321f472558a563d24a
Tomine Odegard <tomineodegard@hotmail.no>
2021-04-20 21:37:21 +0200

    working on JavaScript

---
+++ spil.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", showPage);
+
+function showPage() {
+    console.log("show page");
+
+    document.querySelector("#figur1_container").classList.add("jump");
+    document.querySelector("#figur1_container").addEventListener("mousedown", clickFigur1Handler);
+
+}
+
+
+function clickFigur1Handler() {
+    console.log("clickFigur1Handler");
+    document.querySelector("#figur1_container").removeEventListener("mousedown", clickFigur1Handler);
+    document.querySelector("#figur1_container").classList.add("pause");
+
+    document.querySelector("#figur1_container").classList.add("rotate_after_click");
+}
+
+
+
+/* window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+
+
+    document.querySelector("#red_container").classList.add("pos1");
+    document.querySelector("#red_container").classList.add("flyv");
+    document.querySelector("#red_container").addEventListener("mousedown", clickRedHandler);
+
+}
+
+function clickRedHandler() {
+    console.log("clickRedHandler");
+    document.querySelector("#red_container").removeEventListener("mousedown", clickRedHandler);
+    document.querySelector("#red_container").classList.add("frys");
+    document.querySelector("#red_sprite").classList.add("forsvind");
+    document.querySelector("#red_container").addEventListener("animationend", redReset);
+}
+
+function redReset() {
+    console.log("redReset");
+    document.querySelector("#red_container").classList = "";
+    document.querySelector("#red_sprite").classList = "";
+    document.querySelector("#red_container").removeEventListener("animationend", redReset)
+
+    document.querySelector("#red_container").offsetLeft;
+    document.querySelector("#red_container").classList.add("pos2");
+    document.querySelector("#red_container").classList.add("flyv");
+    document.querySelector("#red_container").addEventListener("mousedown", clickRedHandler);
+}
+
+*/
